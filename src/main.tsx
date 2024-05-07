276446d7692360cd88e9d555fc419cafb3d0e6ac
YashAgarwal1201 <legoyashx@gmail.com>
2024-05-07 23:34:28 +0530

    chore: primary components

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+// import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
+import Router from "./Routes/routes.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={Router} />
   </React.StrictMode>
 );
