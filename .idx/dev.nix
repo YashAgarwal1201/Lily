1579ea2c2fad8cc710f69c1de7e055abb59571ca
legoyashx <legoyashx@gmail.com>
2024-05-11 18:24:29 +0000

    chore: idx commit

---
+++ .idx/dev.nix
@@ -0,0 +1,27 @@
+{pkgs}: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodejs_20 pkgs.yarn
+  ];
+  idx.extensions = [
+    "svelte.svelte-vscode"
+    "vue.volar"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
